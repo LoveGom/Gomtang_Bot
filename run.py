1acefc5de82b466afdbcea0525c9e86175fa91bc
LoveGom <qpig9302@naver.com>
2021-08-05 00:45:45 +0900

    마춤뻡

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ token = token[:-1]
 try:
     startup(token)
 except discord.errors.LoginFailure:
-    print("로그인에 실패했어요 유효한 토크이거나, 토큰에 큰 따옴표를 붙였는지 확인해주세요.")
+    print("로그인에 실패했어요 유효한 토큰이거나, 토큰에 큰 따옴표를 붙였는지 확인해주세요.")
     exit()
